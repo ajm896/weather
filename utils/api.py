69312822b9b61f9d45035d3372f0e62a0e0e4db1
ajm896 <amorris@amorris.cc>
2025-06-20 15:44:23 -0400

    feat(api): add functions to update and fetch gridpoint forecast data
    - Introduced `update_all_forecasts` to fetch and cache forecast data from the NWS API.
    - Added `fetch_gridpoint_raw_data` to retrieve raw gridpoint data.
    - Updated import statements for clarity.
    
    feat(models): enhance gridpoint data models
    
    - Added `GridpointQuantitativeValueLayer` for handling quantitative value layers.
    - Introduced `Gridpoint` model to represent raw forecast data for grid squares.
    - Created `GridpointGeoJson` model for GeoJSON representation of gridpoint data.
    - Included detailed documentation for new models and their attributes.

--- utils/api.py
+++ utils/api.py
@@ -1,10 +1,24 @@
-import requests, json
-import pandas as pd
+import requests
+import json
 
 NWS_X = 40
 NWS_Y = 68
 BASE_URL = f"https://api.weather.gov/gridpoints/GSP/{NWS_X},{NWS_Y}/"
 USER_AGENT = "weather-learner/1.0"
+
+
+def update_all_forecasts() -> None:
+    """Update all forecast data by fetching from the NWS API."""
+    print("Fetching latest forecast data...")
+    forecast_data = fetch_forecast()
+    hourly_forecast_data = fetch_hourly_forecast()
+    gridpoint_raw_data = fetch_gridpoint_raw_data()
+
+    cache_forecast(forecast_data, "forecast.json")
+    cache_forecast(hourly_forecast_data, "hourly_forecast.json")
+    cache_forecast(gridpoint_raw_data, "gridpoint_raw.json")
+
+    print("All forecasts updated.")
 
 
 def fetch_forecast() -> dict[str, dict]:
@@ -19,6 +33,11 @@ def fetch_hourly_forecast() -> dict[str, dict]:
     return requests.get(
         BASE_URL + "forecast", headers={"User-Agent": USER_AGENT}
     ).json()
+
+
+def fetch_gridpoint_raw_data() -> dict[str, dict]:
+    """Fetch the latest gridpoint forecast data from the NWS API."""
+    return requests.get(BASE_URL, headers={"User-Agent": USER_AGENT}).json()
 
 
 def load_cached_data(filename: str) -> dict[str, dict]:
--- utils/models.py
+++ utils/models.py
@@ -1,3 +1,4 @@
+from typing import Any
 from pydantic import BaseModel
 
 
@@ -97,3 +98,105 @@ class GridpointHourlyForecastGeoJson(BaseModel):
     geometry: GeoJsonGeometry
     properties: GridpointHourlyForecast
     type: str
+
+
+class GridpointQuantitativeValueLayer(BaseModel):
+    """A layer of quantitative value data for a gridpoint."""
+
+    uom: str
+    values: list[dict[str, int | float | str]]
+
+    def __str__(self) -> str:
+        return f"{self.values} {self.uom}"
+
+
+class Gridpoint(BaseModel):
+    """
+    Raw forecast data for a 2.5km grid square.
+    This is a list of all potential data layers that may appear.
+    Some layers may not be present in all areas.
+
+    temperature
+    dewpoint
+    maxTemperature
+    minTemperature
+    relativeHumidity
+    apparentTemperature
+    heatIndex
+    windChill
+    wetBulbGlobeTemperature
+    skyCover
+    windDirection
+    windSpeed
+    windGust
+    weather
+    hazards: Watch and advisory products in effect
+    probabilityOfPrecipitation
+    quantitativePrecipitation
+    iceAccumulation
+    snowfallAmount
+    snowLevel
+    ceilingHeight
+    visibility
+    transportWindSpeed
+    transportWindDirection
+    mixingHeight
+    hainesIndex
+    lightningActivityLevel
+    twentyFootWindSpeed
+    twentyFootWindDirection
+    waveHeight
+    wavePeriod
+    waveDirection
+    primarySwellHeight
+    primarySwellDirection
+    secondarySwellHeight
+    secondarySwellDirection
+    wavePeriod2
+    windWaveHeight
+    dispersionIndex
+    pressure: Barometric pressure
+    probabilityOfTropicalStormWinds
+    probabilityOfHurricaneWinds
+    potentialOf15mphWinds
+    potentialOf25mphWinds
+    potentialOf35mphWinds
+    potentialOf45mphWinds
+    potentialOf20mphWindGusts
+    potentialOf30mphWindGusts
+    potentialOf40mphWindGusts
+    potentialOf50mphWindGusts
+    potentialOf60mphWindGusts
+    grasslandFireDangerIndex
+    probabilityOfThunder
+    davisStabilityIndex
+    atmosphericDispersionIndex
+    lowVisibilityOccurrenceRiskIndex
+    stability
+    redFlagThreatIndex"""
+
+    updateTime: str
+    validTimes: str
+    elevation: QuantitativeValue
+    forecastOffice: str
+    gridId: str
+    gridX: int
+    gridY: int
+    temperature: GridpointQuantitativeValueLayer | None = None
+    dewpoint: GridpointQuantitativeValueLayer | None = None
+    maxTemperature: GridpointQuantitativeValueLayer | None = None
+    minTemperature: GridpointQuantitativeValueLayer | None = None
+    relativeHumidity: GridpointQuantitativeValueLayer | None = None
+    apparentTemperature: GridpointQuantitativeValueLayer | None = None
+
+
+class GridpointGeoJson(BaseModel):
+    """A GeoJSON feature for gridpoint data."""
+
+    geometry: GeoJsonGeometry
+    properties: Gridpoint
+
+    type: str
+
+    def __str__(self) -> str:
+        return f"GridpointGeoJson(type={self.type}, properties={self.properties})"
