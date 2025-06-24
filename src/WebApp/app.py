8dbb745e8e5cf3804739675d31937fe904f95606
amorris <amorris@amorris.cc>
2025-06-24 11:15:48 -0400

    Implement weather API endpoint and add HTML interface for data display

--- src/WebApp/app.py
+++ src/WebApp/app.py
@@ -1,8 +1,13 @@
+from pathlib import Path
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from Weather import api, models
 
 app = FastAPI()
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
+html_dir = PROJECT_ROOT / "static" / "html"
 
 
 def get_weather_data() -> models.ForecastData | None:
@@ -12,10 +17,13 @@ def get_weather_data() -> models.ForecastData | None:
     return api.load_cached_data("work_CACHED_FORCAST_DATA.json")
 
 
-@app.get("/")
-async def read_root():
+@app.get("/api/weather")
+async def weather_api():
     weatherData = get_weather_data()
     if weatherData is None:
         return {"error": "No weather data available."}
     forecast_data = weatherData.getForecast()
     return {"weatherData": forecast_data}
+
+
+app.mount("/", StaticFiles(directory=str(html_dir), html=True), name="static")
