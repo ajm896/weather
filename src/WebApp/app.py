0ed932a71ade8356b14b2ab901bc1f2ab3ea8427
amorris <amorris@amorris.cc>
2025-06-26 13:37:51 -0400

    Add CORS middleware and update API endpoint paths for consistency

--- src/WebApp/app.py
+++ src/WebApp/app.py
@@ -1,13 +1,19 @@
-from pathlib import Path
 from fastapi import FastAPI
-# from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from weather import api, models
 
 app = FastAPI()
 
-PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
-html_dir = PROJECT_ROOT / "static"
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://weather.localhost"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def get_weather_data(location: str) -> models.ForecastData | None:
@@ -17,7 +23,7 @@ def get_weather_data(location: str) -> models.ForecastData | None:
     return api.load_cached_data(f"{location}_CACHED_FORECAST_DATA.json")
 
 
-@app.get("/api/weather/{location}")
+@app.get("/v1/api/weather/{location}")
 async def weather_api(location: str):
     weatherData = get_weather_data(location)
     if weatherData is None:
@@ -26,13 +32,10 @@ async def weather_api(location: str):
     return {"weatherData": forecast_data}
 
 
-@app.get("/api/weather/{location}/update")
+@app.get("/v1/api/weather/{location}/update")
 async def update_weather_api(location: str):
     """
     Updates the weather data for a given location.
     """
     api.update_all_forecasts(location=location)
     return {"message": "Weather data updated successfully."}
-
-
-# app.mount("/", StaticFiles(directory=str(html_dir), html=True), name="static")
